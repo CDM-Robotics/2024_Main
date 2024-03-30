840a9d3b0f59cf3ca2af8169140719909e8cd35b
Michael Murphy <mike@sugarmillgang.com>
2024-03-30 13:09:04 -0700

    Add missing AMPScore class

---
+++ src/main/java/frc/robot/auto/AMPScore.java
@@ -0,0 +1,40 @@
+package frc.robot.auto;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
+import frc.robot.commands.DriveAlignToAngle;
+import frc.robot.commands.DriveStraightCommand;
+import frc.robot.commands.EngineerCommand;
+import frc.robot.commands.GangedFireCommand;
+import frc.robot.commands.ResetOdometryCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.GangedMotorSubsystem;
+
+public class AMPScore extends SequentialCommandGroup {
+    public AMPScore(boolean yinverted, DriveSubsystem driveSubsystem, GangedMotorSubsystem gc, Trajectories trajectories) {
+        double invert = 1.0;
+
+        if(yinverted) {
+            invert = -1.0;
+        }
+        this.setName("BLUE AMP Score");
+
+        Pose2d firstPose = new Pose2d(new Translation2d(Units.inchesToMeters((72.5 -35.0) / 2.0), Units.inchesToMeters(invert * (63.5 - 17.5) / 2.0)), new Rotation2d(0.0));
+        Pose2d secondPose = new Pose2d(new Translation2d(Units.inchesToMeters((72.5 - 35.0)), Units.inchesToMeters(invert * (63.5 - 17.5))), new Rotation2d(0.0));
+        Pose2d thirdPose = new Pose2d(new Translation2d(Units.inchesToMeters(21.0 * 12.0), Units.inchesToMeters(invert * (63.5 - 17 - 2))), new Rotation2d(0.0));
+        
+        addCommands(
+            new DriveStraightCommand("AMP Score Midpoint", driveSubsystem, firstPose, Constants.MAX_VELOCITY, Constants.SLOW_VELOCITY, 270.0),
+            new DriveStraightCommand("AMP Score at AMP", driveSubsystem, secondPose, Constants.MAX_VELOCITY, Constants.SLOW_VELOCITY, 270.0),
+            new DriveStraightCommand("AMP Score Drive Out", driveSubsystem, thirdPose, Constants.MAX_VELOCITY, Constants.SLOW_VELOCITY, 0.0)
+        );
+    }
+}
